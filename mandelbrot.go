19a267a4a3f080018fc87821245fafbd3368fd8e
MicheleFiladelfia <michele270904@gmail.com>
2022-08-26 10:50:17 +0200

    refactoring

--- mandelbrot.go
+++ mandelbrot.go
@@ -7,17 +7,17 @@ const (
 	YMIN = -1.6
 )
 
-func mandelbrot(cr float64, ci float64 , maxIterations int) int {
-  var zr float64
-  var zi float64
-  iterations:=0
-  var temp float64
-  for zr * zr + zi * zi < 4 && iterations < maxIterations {
-    temp = zr * zr - zi * zi + cr
-    zi = 2.0 * zr * zi + ci
-    zr = temp
-    iterations++
-  }
+func mandelbrot(cr float64, ci float64, maxIterations int) int {
+	var zr float64
+	var zi float64
+	iterations := 0
+	var temp float64
+	for zr*zr+zi*zi < 4 && iterations < maxIterations {
+		temp = zr*zr - zi*zi + cr
+		zi = 2.0*zr*zi + ci
+		zr = temp
+		iterations++
+	}
 
-  return iterations
+	return iterations
 }
--- update.go
+++ update.go
@@ -3,7 +3,6 @@ package main
 import tea "github.com/charmbracelet/bubbletea"
 
 func (m *model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		switch msg.String() {
